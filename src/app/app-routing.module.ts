6decacf5dcc2cf2cda38f34e79e06ab958fd90c1
SachinClavrit <sachin@clavrit.com>
2022-07-16 12:18:37 +0530

    people table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { LeaveComponent } from './leave/leave/leave.component';
 import { LoginComponent } from './login/login/login.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard/user-dashboard.component';
 import { AboutComponent } from './core-element/about/about.component';
+import { PeopleComponent } from './people/people.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'leave', component: LeaveComponent },
   { path: 'user-dashboard', component: UserDashboardComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path : 'people', component:PeopleComponent}
 ];
 
 @NgModule({
