8869be4b1a0915442fa23d5b3a25bcb91cb09759
meenakshichhaunker <meenakshi@clarvit.com>
2022-07-28 08:54:31 +0530

    in progress

--- src/app/core-element/navigation/navigation.component.ts
+++ src/app/core-element/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
